f91b8a269a6b51cc6c2ba276cba8a4debff8afed
restjohn <restjohn@users.noreply.github.com>
2017-02-20 12:53:41 -0700

    initial commit

---
+++ src/main/java/com/jebrains/youtrack/idea168464/ClassWithNameOfResourceFolder.java
@@ -0,0 +1,8 @@
+package com.jebrains.youtrack.idea168464;
+
+
+public class ClassWithNameOfResourceFolder {
+
+    public static final String importMeStatically = "doesn't work";
+
+}
---
+++ src/main/java/com/jebrains/youtrack/idea168464/Main.java
@@ -0,0 +1,9 @@
+package com.jebrains.youtrack.idea168464;
+
+import static com.jebrains.youtrack.idea168464.ClassWithNameOfResourceFolder.importMeStatically;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(importMeStatically`);
+    }
+}
