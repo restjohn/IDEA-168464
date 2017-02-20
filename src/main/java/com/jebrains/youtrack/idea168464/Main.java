9ff427af8c4ab952f780469d78cc41d3dfdda03b
restjohn <restjohn@users.noreply.github.com>
2017-02-20 13:05:58 -0700

    fix bad field reference

--- src/main/java/com/jebrains/youtrack/idea168464/Main.java
+++ src/main/java/com/jebrains/youtrack/idea168464/Main.java
@@ -1,9 +1,9 @@
 package com.jebrains.youtrack.idea168464;
 
-import static com.jebrains.youtrack.idea168464.ClassWithNameOfResourceFolder.staticImportFieldFails;
+import static com.jebrains.youtrack.idea168464.ClassWithNameOfResourceFolder.staticImportMemberFails;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(staticImportFieldFails);
+        System.out.println(staticImportMemberFails);
     }
 }
