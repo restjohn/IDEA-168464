be997d91212e35b4b52b73f6993d0f851ef25f19
restjohn <restjohn@users.noreply.github.com>
2017-02-20 13:04:02 -0700

    slight rename of static field

--- src/main/java/com/jebrains/youtrack/idea168464/ClassWithNameOfResourceFolder.java
+++ src/main/java/com/jebrains/youtrack/idea168464/ClassWithNameOfResourceFolder.java
@@ -3,6 +3,6 @@ package com.jebrains.youtrack.idea168464;
 
 public class ClassWithNameOfResourceFolder {
 
-    public static final String importMeStatically = "doesn't work";
+    public static final String staticImportMemberFails = "doesn't work";
 
 }
--- src/main/java/com/jebrains/youtrack/idea168464/Main.java
+++ src/main/java/com/jebrains/youtrack/idea168464/Main.java
@@ -1,9 +1,9 @@
 package com.jebrains.youtrack.idea168464;
 
-import static com.jebrains.youtrack.idea168464.ClassWithNameOfResourceFolder.importMeStatically;
+import static com.jebrains.youtrack.idea168464.ClassWithNameOfResourceFolder.staticImportFieldFails;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(importMeStatically`);
+        System.out.println(staticImportFieldFails);
     }
 }
